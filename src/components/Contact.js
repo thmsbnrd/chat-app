0139d2f52bd9b9474a2624cf23666bc25151cf54
Thomas BENARD <thomas0benard@gmail.com>
2020-04-01 13:04:41 +0200

    added divs &  classes in Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import './Contact.css';
 
+const name = "Landon Dixon";
+const avatar = 'https://randomuser.me/api/portraits/men/82.jpg';
+const online = true;
 
 function Contact() {
   return (
-    <div class="Contact">
-      
+    <div className="Contact">
+        <img class="avatar" src={avatar} />
+        <div>
+            <h4 class="name">{name}</h4>
+            <div class="status">
+                <div class="status-online"></div>
+                <div class="status-offline"></div>
+                <p class="status-text">{online ? "Online" : "Offline"}</p>
+            </div>
+        </div>
     </div>
   );
 }
 
 export default Contact;
+
+/*
+const name = "Darth Plagueis";
+const avatar = 'https://avatarfiles.alphacoders.com/798/79894.jpg';
+const online = false;
+const element = (
+  <div className="person-item">
+    <img className ="avatar" src={avatar} />
+    <div>
+      <h4>{name}</h4>
+      <p>{online ? "Online" : "Offline"}</p>
+    </div>
+  </div>
+);
+*/
