b1b7912934911975775457be64f46a1b708c0ee9
Thomas BENARD <thomas0benard@gmail.com>
2020-04-13 18:12:28 +0200

    mapped Contact.js inside ContactList.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact'
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact
-      name="Landon Dixon"
-      avatar='https://randomuser.me/api/portraits/men/82.jpg'
-      online
-      />
-      <Contact
-      name="Sarah Campbell"
-      avatar='https://randomuser.me/api/portraits/women/52.jpg'
-      />
-      <Contact
-      name="Darrell Phillips"
-      avatar='https://randomuser.me/api/portraits/men/5.jpg'
-      online
-      />
+      <ContactList />
     </div>
   );
 }
