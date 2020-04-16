8afd04b91fc9ff411bd5d933baced7fb06531e66
Thomas BENARD <thomas0benard@gmail.com>
2020-04-16 17:25:55 +0200

    added changing online state onClick

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ import './Contact.css';
 class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { online: false}
+    this.state = { online:false}
   }
 
   render() {
@@ -14,9 +14,15 @@ class Contact extends React.Component {
         <img className="avatar" src={this.props.avatar} alt={this.props.name} />
         <div>
             <h4 className="name">{this.props.name}</h4>
-            <div className="status">
-                <div className={this.props.online ? "status-online" : "status-offline"}></div>
-                <p className="status-text">{this.props.online ? "Online" : "Offline"}</p>
+            <div
+            onClick={event => {
+              const newOnline = !this.state.online;
+              this.setState({ online: newOnline });
+            }}
+            className="status"
+            >
+                <div className={this.state.online ? "status-online" : "status-offline"}></div>
+                <p className="status-text">{this.state.online ? "Online" : "Offline"}</p>
             </div>
         </div>
       </div>
